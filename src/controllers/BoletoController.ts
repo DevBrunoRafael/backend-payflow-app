078dd28c03fd6be3771deb5e10c25fa98a4711c5
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-11-16 00:33:29 -0300

    refactor: removing try blocks

--- src/controllers/BoletoController.ts
+++ src/controllers/BoletoController.ts
@@ -3,86 +3,38 @@ import BoletoRepository from "../repositories/BoletoRepository";
 
 export class BoletoController {
    async listarTodosBoletos(req: Request, res: Response) {
-      try {
-         const boletos = await BoletoRepository.findAll();
-         return res.status(200).json(boletos);
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao buscar todos os boletos",
-         });
-      }
+      const boletos = await BoletoRepository.findAll();
+      return res.status(200).json(boletos);
    }
 
    async buscarBoletoPorId(req: Request, res: Response) {
-      try {
-         const boleto = await BoletoRepository.findOne(req.params._id);
-         return res.status(200).json(boleto);
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao buscar boleto por id",
-         });
-      }
+      const boleto = await BoletoRepository.findOne(req.params._id);
+      return res.status(200).json(boleto);
    }
 
    async criarNovoBoleto(req: Request, res: Response) {
-      try {
-         const { nomeBoleto, vencimento, valor, codigo } = req.body;
-         await BoletoRepository.create({
-            nomeBoleto,
-            vencimento,
-            valor,
-            codigo,
-         });
-         return res.status(201).json({
-            message: "Boleto cadastrado!",
-         });
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao cadastar boleto",
-         });
-      }
+      const { nomeBoleto, vencimento, valor, codigo } = req.body;
+      await BoletoRepository.create({
+         nomeBoleto,
+         vencimento,
+         valor,
+         codigo,
+      });
+      return res.status(201).json({ message: "Boleto cadastrado!" });
    }
 
    async deletarBoleto(req: Request, res: Response) {
-      try {
-         await BoletoRepository.deleteOne(req.params._id);
-         return res.status(200).json({
-            message: "Boleto deletado!",
-         });
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao deletar boleto",
-         });
-      }
+      await BoletoRepository.deleteOne(req.params._id);
+      return res.status(200).json({ message: "Boleto deletado!" });
    }
 
    async marcarComoPago(req: Request, res: Response) {
-      try {
-         await BoletoRepository.update(req.params._id);
-         return res.status(200).json({
-            message: "Boleto pago!",
-         });
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao atualizar status do boleto",
-         });
-      }
+      await BoletoRepository.update(req.params._id);
+      return res.status(200).json({ message: "Boleto pago!" });
    }
 
    async listarBoletosPagos(req: Request, res: Response) {
-      try {
-         const boletosPagos = await BoletoRepository.findAllPaid();
-         return res.status(200).json(boletosPagos);
-      } catch (error) {
-         console.log(error);
-         return res.status(500).json({
-            message: "erro ao buscar todos os boletos",
-         });
-      }
+      const boletosPagos = await BoletoRepository.findAllPaid();
+      return res.status(200).json(boletosPagos);
    }
 }
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,83 +2,56 @@ import { Request, Response } from "express";
 import UserRepository from "../repositories/UserRepository";
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
+import { BadRequestError, UnouthorizedError } from "helpers/api-errors";
 
 export class UserController {
    async createUser(req: Request, res: Response) {
-      try {
-         const { nome, email, password } = req.body;
+      const { nome, email, password } = req.body;
 
-         const emailInUSe = await UserRepository.findUserByEmail(
-            req.params.email
-         );
-         if (emailInUSe) {
-            res.status(403).json({ message: "Email em uso" });
-            throw new Error();
-         }
+      const emailInUSe = await UserRepository.findUserByEmail(req.params.email);
 
-         await UserRepository.create({
-            nome,
-            email,
-            password,
-         });
-         res.status(201).json({
-            message: "Usuário cadastrado!",
-         });
-      } catch (error) {
-         console.log(error);
-         res.status(500).json({
-            message: "Erro ao criar usuário!",
-         });
+      if (emailInUSe) {
+         throw new BadRequestError("Email em uso.");
       }
+
+      await UserRepository.create({
+         nome,
+         email,
+         password,
+      });
+
+      return res.status(201).json({ message: "Usuário cadastrado!" });
    }
 
    async login(req: Request, res: Response) {
-      try {
-         const { email, password } = req.body;
+      const { email, password } = req.body;
 
-         const user = await UserRepository.findUserByEmail(email);
+      const user = await UserRepository.findUserByEmail(email);
 
-         if (!user) {
-            res.status(403).json({ message: "Email inválido" });
-            throw new Error();
-         }
-
-         const verifyPassword = await bcrypt.compare(
-            password,
-            user.password ?? ""
-         );
-
-         console.log(user.password);
-         console.log(verifyPassword);
-
-         if (!verifyPassword) {
-            // concertar validação do password
-            res.status(403).json({ message: "Senha inválida" });
-            throw new Error();
-         }
-
-         const token = jwt.sign(
-            { _id: user.id },
-            process.env.SECRET_KEY ?? "",
-            { expiresIn: "7d" }
-         );
-
-         res.status(200).json({
-            user: { nome: user.nome },
-            token: token,
-         });
-      } catch (error) {
-         console.log(error);
-         res.status(500).json({ message: "Erro ao logar" });
+      if (!user) {
+         throw new BadRequestError("Email não cadastrado.");
       }
+
+      const verifyPassword = await bcrypt.compare(
+         password,
+         user.password ?? ""
+      );
+
+      if (!verifyPassword) {
+         throw new BadRequestError("Senha incorreta.");
+      }
+
+      const token = jwt.sign({ _id: user.id }, process.env.SECRET_KEY ?? "", {
+         expiresIn: "7d",
+      });
+
+      res.status(200).json({
+         user: { nome: user.nome },
+         token: token,
+      });
    }
 
    async getTest(req: Request, res: Response) {
-      try {
-         res.status(200).json(req.user);
-      } catch (error) {
-         console.log(error);
-         res.status(500).json({ message: "Usuáio não autorizado" });
-      }
+      res.status(200).json(req.user);
    }
 }
